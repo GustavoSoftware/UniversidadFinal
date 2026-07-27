55f36abe13ac2041337ef0984e07c1bf9e09e603
JoelAndreAnyoza <joelanyoza2004@gmail.com>
2026-07-27 18:57:51 -0500

    Clean up comments in postgres.ts
    Removed unnecessary comments from postgres.ts file.

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -1,8 +1,5 @@
 import { Pool } from "pg";
 
-//pool desde src usando variables de entorno
-//este archivo ha sido creado con la finalidad de replicar el trabajo del profesor
-
 export const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
