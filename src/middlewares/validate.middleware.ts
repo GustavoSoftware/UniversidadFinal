bf5448fe45f2f9c78bb59376e498e79a73a5961e
JoelAndreAnyoza <joelanyoza2004@gmail.com>
2026-07-27 18:59:16 -0500

    Clean up validate.middleware.ts by removing comments
    Removed commented-out code for the validate middleware function.

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -3,22 +3,6 @@ import { STATUS_BAD_REQUEST } from "../shared/constants";
 import { BaseResponse } from "../shared/base-response";
 import { ObjectSchema } from "joi";
 
-// export const validate =
-//   (schema: ObjectSchema, property: "body" | "params" | "query") =>
-//   (req: Request, res: Response, next: NextFunction) => {
-//     const { error } = schema.validate(req[property], {
-//       abortEarly: false,
-//     });
-
-//     if (error) {
-//       const messages = error.details.map((d) => d.message);
-//       return res
-//         .status(STATUS_BAD_REQUEST)
-//         .json(BaseResponse.error(messages.join(", ")));
-//     }
-
-//     next();
-//   };
 
 export const validate =
   (schema: ObjectSchema, property: "body" | "params" | "query") =>
